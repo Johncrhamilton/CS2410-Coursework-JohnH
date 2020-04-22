eda34b393c805bfcfa37ccc3671dcc4ec2d2e0ee
John <john.crhamilton@gmail.com>
2020-04-22 16:13:10 +0100

    Verify Email with MailTrap

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function ()
+{
   return view('welcome');
 });
 
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('display_user',
 'UserController@display')->name('display_user');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
