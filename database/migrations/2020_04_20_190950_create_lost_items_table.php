3ee3f297f549a572c9d693c2f2de83adfcf609bf
John <john.crhamilton@gmail.com>
2020-05-05 22:10:13 +0100

    Update 2020_04_20_190950_create_lost_items_table.php

--- database/migrations/2020_04_20_190950_create_lost_items_table.php
+++ database/migrations/2020_04_20_190950_create_lost_items_table.php
@@ -21,7 +21,7 @@ class CreateLostitemsTable extends Migration
       $table->unsignedInteger('found_user')->nullable();
       $table->string('found_place',80);
       $table->string('colour',40);
-      $table->binary('image')->nullable();
+      $table->string('image')->nullable();
       $table->text('description');
       $table->timestamps();
     });
